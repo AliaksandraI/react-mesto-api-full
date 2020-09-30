270042da112fcb019c922a246bfa2a1eb4315318
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-09-30 21:25:27 +0200

    backend done

--- app.js
+++ app.js
@@ -19,16 +19,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-/*
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5f4d4ad9432f43ee09bd7620',
-  };
-
-  next();
-});
-*/
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
