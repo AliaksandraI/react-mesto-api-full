a615fa02ba2e5cd7f7bb4a87d764f3e6f958679b
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-10-02 19:13:48 +0200

    auth is updated

--- routes/users.js
+++ routes/users.js
@@ -10,17 +10,10 @@ router.get('/', getUsers);
 
 router.get('/:id', getProfile);
 
-router.patch('/me',celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-  }).unknown(true),
-}),  updateProfile);
+router.patch('/me', updateProfile);
 
 router.patch('/me/avatar',celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
     avatar: Joi.string().required().pattern(/(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/),
   }).unknown(true),
 }),  updateAvatarProfile);
